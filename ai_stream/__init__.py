d76ad99b4f9916579b3004cc0ad34630b6b1937f
Shaojie Jiang <shaojie.jiang1@gmail.com>
2024-09-28 23:31:40 +0200

    Minor.

--- ai_stream/__init__.py
+++ ai_stream/__init__.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TESTING = os.environ.get("TESTING", "false").lower() == "true"
-LOCAL_AWS = os.environ.get("LOCAL_AWS", "false").lower() == "true"
+LOCAL_AWS = os.environ.get("LOCAL_AWS", "true").lower() == "true"
 
 __author__ = "AI Colleagues"
 __email__ = "info@ai-colleagues.com"
