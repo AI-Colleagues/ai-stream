825cf4da1b7e4d49ed62fc750fb399bec65b9cf0
Shaojie Jiang <shaojie.jiang1@gmail.com>
2024-09-29 01:09:39 +0200

    Added aws region.

--- ai_stream/db/aws.py
+++ ai_stream/db/aws.py
@@ -66,6 +66,7 @@ def _prepare_dev_env() -> None:
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
 
 
 def create_tables() -> None:
