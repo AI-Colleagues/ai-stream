5a8074a6bd313f4b42863f7c6472039b01354c41
Shaojie Jiang <shaojie.jiang1@gmail.com>
2024-10-06 11:38:13 +0200

    Store preserved tools to DB.

--- ai_stream/components/tools.py
+++ ai_stream/components/tools.py
@@ -4,6 +4,12 @@ from collections.abc import Callable
 from langchain_core.tools import BaseTool
 from langchain_core.utils.function_calling import convert_to_openai_function
 from pydantic import BaseModel
+from pynamodb.exceptions import DoesNotExist
+from ai_stream.config import get_logger
+from ai_stream.db.aws import FunctionsTable
+
+
+logger = get_logger(__name__)
 
 
 class Tool(BaseTool):
@@ -19,7 +25,20 @@ TOOLS: dict[str, type[Tool]] = {}
 
 def register_tool(cls: type[Tool]) -> Callable:
     """Register a tool."""
-    TOOLS[cls.__name__] = cls
+    tool_name = cls.__name__
+    TOOLS[tool_name] = cls
+    schema_cls = getattr(cls, f"{tool_name}Schema")
+    schema = convert_to_openai_function(schema_cls)
+    try:
+        item = FunctionsTable.get(tool_name)  # A preserved tool uses its name as ID
+        if schema != item.value.as_dict():
+            item.update(actions=[FunctionsTable.value.set(schema)])
+            logger.info(f"Schema for {tool_name} has been updated.")
+    except DoesNotExist:
+        item = FunctionsTable(id=tool_name, name=tool_name, used_by=[], value=schema)
+        item.save()
+        logger.info(f"Stored new schema {tool_name}.")
+
     return cls
 
 
